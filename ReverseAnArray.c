d3bcf9b0b6e06d712880f48056e0ad131ff2c72a
Aryns293 <arynshr293@gmail.com>
2024-10-09 10:55:00 +0530

    Add files via upload

---
+++ ReverseAnArray.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int size)
+{
+    int start = 0, end = size - 1;
+    int temp;
+
+    // Swap elements from start and end until the middle of the array
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    printf("Reversed array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
